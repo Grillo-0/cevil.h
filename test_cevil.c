474ab92fd8190c204a6320ab53acffc61b09f2a3
Arthur Grillo <arthurgrillo@riseup.net>
2023-08-25 11:04:48 -0300

    cevil: Return errors instead of exiting

--- test_cevil.c
+++ test_cevil.c
@@ -8,7 +8,16 @@
 #define test(input, expected) test_pos(input, expected, __FILE__, __LINE__)
 
 void test_pos(char* expr, double expected, const char* file, size_t line) {
-	double res = cevil_eval(expr);
+	struct cevil_error err;
+	double res;
+
+	err = cevil_eval(expr, &res);
+
+	if (err.type != CEVIL_ERROK) {
+		printf("[FAILED]");
+		cevil_print_error(&err);
+		exit(-1);
+	}
 
 	if (res == expected) {
 		printf("[PASSSED]: \"%s\" == %f\n", expr, res);
